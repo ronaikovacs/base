f0597d19770847b8b1c988b1e5bd47eacbe61eb8
remo <nicolas.sipek@gmail.com>
2022-05-16 15:41:34 +0200

    I am speed REVISED

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -18,7 +18,6 @@ public class TrainControllerImpl implements TrainController {
 					try {
 						Thread.sleep(1000);
 					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 				}
